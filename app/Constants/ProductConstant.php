2a44491a46136de2cc5a794afcc99c8924c8cf1e
amirhf <amirhacking85@gmail.com>
2023-03-15 14:05:22 +0330

    created product constants

---
+++ app/Constants/ProductConstant.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Constants;
+
+class ProductConstant
+{
+    const TYPE_FLIGHT = 'flight';
+    // add add more ...
+
+    const PRODUCT_TYPES = [
+        self::TYPE_FLIGHT,
+    ];
+}
