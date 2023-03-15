2b2ad5e94305d6b254be454700f3b0ede2dfa7b7
amirhf <amirhacking85@gmail.com>
2023-03-15 17:24:35 +0330

    write test and make migration for RefreshDatabase

---
+++ database/migrations/2023_03_15_170858_create_seo_landings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::create('seo_landings', function (Blueprint $table) {
+            $table->id();
+
+
+            $table->unsignedBigInteger('admin_id');
+            $table->string('product');
+            $table->unsignedBigInteger('origin_id');
+            $table->unsignedBigInteger('destination_id');
+            $table->string('seo_h1');
+            $table->string('seo_title');
+            $table->string('seo_desc');
+            $table->text('content')->nullable();
+            $table->integer('status')->default(1);
+//            $table->timestamps();
+            $table->softDeletes();
+
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('seo_landings');
+    }
+};
---
+++ tests/Feature/WriteDataServiceTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Constants\ProductConstant;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Storage;
+use Symfony\Component\HttpFoundation\Response as ResponseAlias;
+use Tests\TestCase;
+
+class WriteDataServiceTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_write_method_creates_seo_landing_records()
+    {
+        // Arrange
+        $this->withoutExceptionHandling();
+        Storage::fake('public');
+        $expectedGoings = ['1093'];
+        $expectedGoals = ['33'];
+
+        // Act
+        $response = $this->get(route('index'));
+
+        // Assert
+        $response->assertStatus(ResponseAlias::HTTP_OK);
+        foreach ($expectedGoings as $goingId) {
+            foreach ($expectedGoals as $goalId) {
+                $this->assertDatabaseHas('seo_landings', [
+                    'seo_h1' => "بلیط هواپیما $goingId به $goalId",
+                    'origin_id' => $goingId,
+                    'destination_id' => $goalId,
+                    'seo_title' => "بلیط هواپیما $goingId به $goalId | پرواز $goingId $goalId",
+                    'seo_desc' => "خرید آنلاین بلیط هواپیما $goingId $goalId از قاصدک 24 چارتر و سیستمی با پشتیبانی 24ساعته. برای اطلاع از قیمت بلیط ارزان هواپیما $goingId $goalId کلیک کنید.",
+                    'product' => ProductConstant::TYPE_FLIGHT,
+                ]);
+            }
+        }
+
+        $this->assertFileExists(public_path('template/xlsx/new.xlsx'));
+    }
+
+}
+
